08e11c38349a32d93d3d7d4ab03e70b048115ede
Andreas Wassmer <aw@andreas-wassmer.ch>
2013-12-06 08:24:57 +0100

    Added file with very basic help system

---
+++ helpSystem.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+helpSystem.py
+
+Created by Andreas Wassmer on 2013-12-05.
+Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+"""
+
+def short_help():
+	print("Available commands: m - move ship,  d - drop bomp, ")
+	print("                    s - read sonar, a - abandon ship")

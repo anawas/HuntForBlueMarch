fd7910cea411193b31b24bd9a69f6086e1d78c47
Andreas Wassmer <aw@andreas-wassmer.ch>
2013-12-10 07:44:50 +0100

    Can now drop water bombs. There are only 4 directions to move: N, E, S, W

--- HuntForBlueMarch.py
+++ HuntForBlueMarch.py
@@ -12,26 +12,31 @@ import helpSystem
 #import sysCheck
 import numpy
 
-BOARDWIDTH = 20
+BOARDWIDTH  = 20
 BOARDHEIGHT = 20
+SONARRADIUS = 5
+BOMBRADUIS  = SONARRADIUS - 1
 
 theSubmarine = []
 theVessel = []
+directHits = 2
+
+gameOver = False
 
 def main():
 	print("in main()")
-   	initGame()
+	initGame()
 
 	key = 'a'
-	while key != 'q':
-	   	print("Your orders, sir:"),
-   		key = raw_input()
+	while key != 'q' and gameOver == False:
+		print("Your orders, sir:"),
+		key = raw_input()
 		if key == 'm':
 			print("moving vessel")
 			moveShip()
 			print(theVessel)
 		elif key == 'd':
-			print("dropping water bomb")
+			dropBomb()
 		elif key == 's':
 			print("reading sonar")
 			readSonar()
@@ -44,28 +49,38 @@ def main():
 			print("quit game")
 		else:
 			print("Sorry, sir, I don't understand!")
+	
+	if gameOver:
+		print("Congratulation, sir! You completed the mission successfully!")
+	
+	
 	# drawBoard()
 	
 
 def setSubmarine():
-   global theSubmarine
+	"""Sets the initial position of the submarine
+	on the grid. """
+	global theSubmarine
 
-   xpos = numpy.random.randint(0,BOARDWIDTH)
-   ypos = numpy.random.randint(0, BOARDHEIGHT)
-   theSubmarine = [xpos, ypos]
-   print(theSubmarine)
+	xpos = numpy.random.randint(0,BOARDWIDTH)
+	ypos = numpy.random.randint(0, BOARDHEIGHT)
+	theSubmarine = [xpos, ypos]
+	print(theSubmarine)
 
 def setVessel():
-   global theVessel
+	"""Sets the initial position of the vessel
+	on the grid. """
+	
+   	global theVessel
 
-   xpos = numpy.random.randint(0,BOARDWIDTH)
-   ypos = numpy.random.randint(0, BOARDHEIGHT)
+	xpos = numpy.random.randint(0,BOARDWIDTH)
+	ypos = numpy.random.randint(0, BOARDHEIGHT)
    
-   if xpos == theSubmarine[0] and ypos == theSubmarine[1]:
-      xpos = numpy.random.randint(1,BOARDWIDTH)
-      ypos = numpy.random.randint(1, BOARDHEIGHT)
+	if xpos == theSubmarine[0] and ypos == theSubmarine[1]:
+		xpos = numpy.random.randint(1,BOARDWIDTH)
+		ypos = numpy.random.randint(1, BOARDHEIGHT)
    
-   theVessel = [xpos, ypos]
+		theVessel = [xpos, ypos]
 
 def initGame():
 	setSubmarine()
@@ -75,42 +90,65 @@ def initGame():
 def readSonar():
 	base_msg = "Sonar reports: "
 	
-	dx = theVessel[0] - theSubmarine[0]
-	if dx < 0:
-		dx = -dx
-	dy = theVessel[1] - theSubmarine[1]
-	if dy < 0:
-		dy = -dy
-	
-	if dx+dy <= 5:
+	distance = calculateDistance()
+	if distance <= SONARRADIUS:
 		print(base_msg + "submarine detected, sir!")
-	elif dx+dy > 5:
+	else:
 		print(base_msg + "nothing nearby, sir!")
-	
+
+
 def moveShip():
 	global theVessel
-	directions = ["N", "NE", "E", "SE", "S", "SW", "W", "NW"]
+	directions = ["N", "E", "S", "W"]
 	
 	direction_ok = False
 	
-	print("Direction (N,NE,E,SE,S,SW,W,NW): "),
+	print("Direction (N,E,S,W): "),
 	while direction_ok == False:
 		direction = raw_input()
 		
-		if direction == "N" or direction == "n":
+		if direction.upper() == "N":
 			theVessel[1] = theVessel[1]-1;
 			direction_ok = True
-		elif direction == "S" or direction == "s":
+		elif direction.upper() == "S":
 			theVessel[1] = theVessel[1]+1;
 			direction_ok = True
-		elif direction == "E" or direction == "e":
+		elif direction.upper() == "E":
 			theVessel[0] = theVessel[0]+1;
 			direction_ok = True
-		elif direction == "W" or direction == "w":
+		elif direction.upper() == "W":
 			theVessel[0] = theVessel[0]-1;
 			direction_ok = True
 	
+def dropBomb():
+	global directHits
 	
+	print("Dropping bomb at %d, %d" % (theVessel[0], theVessel[1]))
+	distance = calculateDistance()
+	
+	if distance <= BOMBRADUIS:
+		print("Direct hit, sir!")
+		--directHits
+	
+	if directHits == 0:
+		print("We sunk her, sir!")
+		gameOver = True
+		 		
+
+def calculateDistance():
+	"""Calculates the Manhattan distance between the vessel and
+	the submarine. Returns this distance."""
+	
+	dx = theVessel[0] - theSubmarine[0]
+	if dx < 0:
+		dx = -dx
+	
+	dy = theVessel[1] - theSubmarine[1]
+	if dy < 0:
+		dy = -dy
+		
+	print("distance = %d" % (dx+dy))
+	return dx+dy
 	
 
 if __name__ == '__main__':
